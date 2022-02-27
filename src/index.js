497aafa7358963a090c997a2ea5deb6924c240e5
brianpompey <brianthescholar@gmail.com>
2022-02-27 10:58:52 -0500

    working

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ const recipeRoutes = require('./routes/recipeRoutes');
 // const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
+app.use(bodyParser.json());
+// app.use(recipeRoutes);
 
-const mongoUri = 'mongodb+srv://brianpompey:password@cluster0.5udqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoUri = 'mongodb+srv://brianpompey:passwordpassword@cluster0.5udqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
