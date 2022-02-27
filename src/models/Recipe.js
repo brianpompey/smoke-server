bff6a2eacb0d3f29034bfdff32fc13614092878c
brianpompey <brianthescholar@gmail.com>
2022-02-27 11:02:58 -0500

    adding recipe schema

--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('Recipe', recipeSchema);
