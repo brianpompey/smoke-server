22123a3c848ce6a9c5a6935c1b9ec61b8408178e
brianpompey <brianthescholar@gmail.com>
2022-02-27 10:19:23 -0500

    working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const recipeRoutes = require('./routes/recipeRoutes');
+// const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
